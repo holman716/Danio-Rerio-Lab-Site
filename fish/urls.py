9cd50073714519225cab65b1cd033bd77a8e26b2
Jay Holman <holman716@gmail.com>
2012-06-01 15:29:22 -0500

    - Addressed URL error

--- fish/urls.py
+++ fish/urls.py
@@ -22,6 +22,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
 )
-
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
