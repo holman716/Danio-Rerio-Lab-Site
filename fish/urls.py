76c4086dfecd4d08ca1e882b2c8a54136a63cf3e
Jay Holman <holman716@gmail.com>
2012-06-01 15:22:55 -0500

    - Another attempt at loading static files

--- fish/urls.py
+++ fish/urls.py
@@ -22,3 +22,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
 )
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
