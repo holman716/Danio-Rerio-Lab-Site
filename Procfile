055bf2c0bfc0ccec95a7f2a70da76130f53fab07
Jay Holman <holman716@gmail.com>
2012-06-01 15:06:14 -0500

    - Attempting to add static files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python fish/manage.py run_gunicorn -b "0.0.0.0:$PORT"
+web: python fish/manage.py collectstatic run_gunicorn -b "0.0.0.0:$PORT"
--- fish/settings.py
+++ fish/settings.py
@@ -1,6 +1,7 @@
 import os.path
 # Django settings for fish project.
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -57,7 +58,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,7 +71,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-	"static",
+	os.path.join(PROJECT_PATH, 'staticfiles'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
